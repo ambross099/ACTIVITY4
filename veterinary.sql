a1be324779981de67c3afe8f029e9bdf11866ba3
Ambross Matibag <ambrossmatibag@gmail.com>
2025-11-22 13:04:57 +0800

    feat/create-table-owners

--- veterinary.sql
+++ veterinary.sql
@@ -1,1 +1,8 @@
 CREATE DATABASE veterinary; 
+CREATE TABLE owners (
+    owner_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    phone_number VARCHAR(15),
+    email VARCHAR(100)
+);  
